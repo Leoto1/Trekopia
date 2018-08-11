45fdc1a2262603901a2f904e636eb262a376a48e
Mayank Ramina <raminamayank16@gmail.com>
2018-08-11 12:38:35 +0530

    Added Comments

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost:27017/Trekopia", { useNewUrlParser: true });
 
+
+var commentSchema = new mongoose.Schema({
+    author: String,
+    content: String
+});
+
+var Comment = mongoose.model("Comment", commentSchema);
+
+
 var trekSchema = new mongoose.Schema({
     name: String,
     cost:Number,
@@ -16,7 +25,11 @@ var trekSchema = new mongoose.Schema({
     days:Number,
     bestTime:String,
     images:String,
-    description: String
+    description: String,
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
 });
 
 var Trek = mongoose.model("Trek", trekSchema);
@@ -30,7 +43,6 @@ app.get("/treks", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            console.log(allTrek);
             res.render("treks", { treks: allTrek });
         }
     });
@@ -53,16 +65,33 @@ app.get("/treks/new", function (req, res) {
 });
 
 app.get("/treks/:id", function (req, res) {
-    Trek.findById(req.params.id, function (err, foundTrek) {
+    Trek.findById(req.params.id).populate("comments").exec(function (err, foundTrek) {
         if (err) {
             console.log(err);
         } else {
-            console.log(foundtrek)
             res.render("show", { trek: foundTrek });
         }
     });
 });
+app.post("/treks/:id/comments", function (req, res) {
+    Trek.findById(req.params.id, function (err, foundTrek) {
+        if (err) {
+            console.log(err);
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    foundTrek.comments.push(comment);
+                    foundTrek.save();
+                    res.redirect("/treks/" + foundTrek._id);
+                }
+            });
+        }
+    });
+});
 
-app.listen(8080, function () {
+
+app.listen(8080,'192.168.1.102' ||'localhost', function () {
     console.log("Trekopia has started");
 });
